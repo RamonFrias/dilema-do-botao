097cc0a58c2253652a5fe956345bf3da529f95fb
Ramon Frias <ramonfrias2451@gmail.com>
2022-06-07 00:27:40 -0300

    Tela de opções criada

--- lib/pages/opcoes.page.dart
+++ lib/pages/opcoes.page.dart
@@ -1,3 +1,4 @@
+import 'package:dilemadobotao/pages/inicial.page.dart';
 import 'package:flutter/material.dart';
 
 class OpcoesPage extends StatefulWidget {
@@ -8,8 +9,93 @@ class OpcoesPage extends StatefulWidget {
 }
 
 class _OpcoesPageState extends State<OpcoesPage> {
+  bool checkValue = true;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(children: <Widget>[
+          const SizedBox(
+            height: 120,
+          ),
+          CheckboxListTile(
+            title: const Text(
+              "Som",
+              style: TextStyle(fontSize: 50),
+            ),
+            activeColor: Colors.red,
+            value: checkValue,
+            onChanged: (bool? value) {
+              setState(() {
+                if (checkValue == true) {
+                  checkValue = false;
+                } else {
+                  checkValue = true;
+                }
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: const Text(
+              "Música",
+              style: TextStyle(fontSize: 50),
+            ),
+            activeColor: Colors.red,
+            value: checkValue,
+            onChanged: (bool? value) {
+              setState(() {
+                if (checkValue == true) {
+                  checkValue = false;
+                } else {
+                  checkValue = true;
+                }
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: const Text(
+              "Vibração",
+              style: TextStyle(fontSize: 50),
+            ),
+            activeColor: Colors.red,
+            value: checkValue,
+            onChanged: (bool? value) {
+              setState(() {
+                if (checkValue == true) {
+                  checkValue = false;
+                } else {
+                  checkValue = true;
+                }
+              });
+            },
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+              backgroundColor:
+                  MaterialStateProperty.resolveWith<Color?>((states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return Colors.red.shade300;
+                }
+                return Colors.red;
+              }),
+              fixedSize: MaterialStateProperty.all(const Size(150, 70)),
+            ),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PaginaInicial()));
+            },
+            child: const Text(
+              "Voltar",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ]));
   }
 }
